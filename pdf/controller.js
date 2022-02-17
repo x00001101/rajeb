b32dd2421c6fd30a68656699aaba67f565d566d2
x00001101 <x00001101@gmail.com>
2022-02-17 14:11:14 +0700

    add print10075

--- pdf/controller.js
+++ pdf/controller.js
@@ -33,7 +33,32 @@ exports.createAwpPrint = async (req, res, next) => {
     req.query.bin = false;
   }
   try {
-    var binaryResult = await pdfModel.createPdfNew(req.query.id);
+    var binaryResult = await pdfModel.createPdf10075(req.query.id);
+    if (!binaryResult) {
+      return res.status(404).send({ message: "Order Id not found!" });
+    }
+    if (req.query.bin) {
+      return res.send(binaryResult);
+    } else {
+      return res.contentType("application/pdf").send(binaryResult);
+    }
+  } catch (err) {
+    res.send(
+      "<h2>There was an error displaying the PDF document.</h2> Error message: " +
+        err.message
+    );
+  }
+};
+
+exports.testPdfAwb = async (req, res, next) => {
+  if (!req.query.id) {
+    return res.status(400).send("Need Id Parameter");
+  }
+  if (!req.query.bin) {
+    req.query.bin = false;
+  }
+  try {
+    var binaryResult = await pdfModel.createPdf10075(req.query.id);
     if (!binaryResult) {
       return res.status(404).send({ message: "Order Id not found!" });
     }
